95afd74847f22a9613d60392042a30052d6b6db1
Kai Zimmermann <kai.zimmermann@audi.de>
2020-02-26 18:45:46 +0100

    test with testdata started

---
+++ app/index.js
@@ -0,0 +1,51 @@
+const puppeteer = require('puppeteer')
+
+const App = {
+    SCHED_PAGE_URL: 'https://sched.com/directory',
+    CONF_DATA_TEMPLATE: {
+        "name": undefined,
+        "url": undefined,
+        "startDate": undefined,
+        "endDate": undefined,
+        "city": undefined,
+        "country": undefined,
+        "twitter": undefined,
+        "cfpUrl": undefined, // call for papers
+        "cfpEndDate": undefined
+    },
+    browser: undefined,
+    async getConferences() {
+        if (this.browser) {
+            await this.browser.close();
+        }
+
+        this.browser = await puppeteer.launch();
+        const page = await this.browser.newPage();
+        await page.goto(this.SCHED_PAGE_URL);
+
+        // could by li.eventtile:has(div.category:contains("Conference")) but Chrome does not support :has
+        const events = await page.$$('li.eventtile');
+
+        // forEach, filter and Co. seems not to support Promises
+        const conferences = []
+        for (const n of events) {
+            const category = await n.$eval('div.category', node => node.innerText);
+            if ('Conference' === category) {
+                conferences.push(n);
+            }
+        }
+        return conferences;
+    },
+    async getConfDataFrom(conference) {
+        const data = this.CONF_DATA_TEMPLATE;
+
+        conference.startDate = await conference.$eval('div.eventname', node => node.innerText);
+
+        return data;
+    },
+    async closeBrowser() {
+        await this.browser.close();
+    }
+}
+
+module.exports = App;
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const puppeteer = require('puppeteer');
+//const sched2conf = require('./app');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://sched.com/directory');
-  const events = await page.$('li.eventtile:has(div.category:contains("Conference"))');
+  const events = await page.$$('li.eventtile');
 
   console.log(events.length);
 
